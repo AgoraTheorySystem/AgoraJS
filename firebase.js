85e8dfbae389288e71627abe7abf65b839d4509a
William Fagundes <william.fagundes.dev@gmail.com>
2025-01-15 18:10:00 -0300

    Cadastro Pronto

---
+++ firebase.js
@@ -0,0 +1,14 @@
+console.log("Firebase Loaded");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsGFmMYzhOmJ4TkUoQmlj2R5R385PWLl8",
+  authDomain: "agoradatabase-17954.firebaseapp.com",
+  projectId: "agoradatabase-17954",
+  databaseURL: "https://agoradatabase-17954-default-rtdb.firebaseio.com",
+  storageBucket: "agoradatabase-17954.firebasestorage.app",
+  messagingSenderId: "759571782330",
+  appId: "1:759571782330:web:1d6045023e59f3d0960083",
+  measurementId: "G-E5S5ZKLJYG"
+};
+
+export default firebaseConfig;
---
+++ index.js
@@ -0,0 +1,46 @@
+import firebaseConfig from './firebase.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js";
+
+// Inicializar o Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Manipulação do formulário de login
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Login bem-sucedido
+            const user = userCredential.user;
+            alert(`Bem-vindo, ${user.email}!`);
+            // Redirecione para outra página se necessário
+            window.location.href = '/home/home.html';
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            let message;
+        
+            switch (errorCode) {
+                case 'auth/wrong-password':
+                    message = 'Senha incorreta. Tente novamente.';
+                    break;
+                case 'auth/user-not-found':
+                    message = 'Usuário não encontrado. Verifique o e-mail.';
+                    break;
+                case 'auth/invalid-email':
+                    message = 'E-mail inválido.';
+                    break;
+                default:
+                    message = 'Erro desconhecido. Tente novamente mais tarde.';
+            }
+        
+            alert(message);
+        });
+});
